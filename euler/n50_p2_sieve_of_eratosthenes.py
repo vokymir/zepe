32afa83c6b7892032cea6cd6f5385b879b9a2a3c
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-03-14 10:28:13 +0100

    sp2 bonus A1 sieve of eratosthenes

---
+++ euler/n50_p2_sieve_of_eratosthenes.py
@@ -0,0 +1,24 @@
+
+
+def sieve_of_eratosthenes(n:int) -> list[int]:
+    array:bytearray = bytearray(b'\x01') * (n + 1)
+    array[:2] = b'\x00\x00'
+    index:int = 0
+    res:list[int] = list()
+
+    while index * index < n:
+
+        if array[index]:
+            array[index * index : n + 1 : index] = b'\x00' * ( (n - index*index) // index + 1 )
+
+        index += 1
+
+    for i in range(len(array)):
+        if array[i]:
+            res.append(i)
+
+    return res
+
+
+if __name__ == "__main__":
+    print(sieve_of_eratosthenes(100))
